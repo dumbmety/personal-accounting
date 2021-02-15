82d745f6f43de6e18265a212331cf080b256e725
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-02-15 18:22:53 +0330

    feat: header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
 
 const Header = () => {
-  return <h1>Header</h1>
+  return (
+    <header className="mb-5">
+      <h1 className="text-5xl font-semibold tracking-tigh py-2">
+        Personal Accounting
+      </h1>
+      <p className="text-lg leading-10 text-gray-600">
+        Created by{' '}
+        <a
+          className="border-b border-gray-300 text-blue-600 font-semibold hover:border-blue-400"
+          href="https://github.com/neysidev"
+          target="_blank"
+          rel="noopener"
+        >
+          Mehdi Neysi
+        </a>
+      </p>
+    </header>
+  )
 }
 
 export default Header
