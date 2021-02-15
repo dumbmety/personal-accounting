da55910e5ac497ef82c0abb8ac3854e4b53e60ea
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-02-15 18:03:10 +0330

    feat: made layout

---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Header from '../components/Header/Header'
+import Main from '../components/Main/Main'
+
+const App = () => {
+  return (
+    <div className="container mx-auto py-16 px-6">
+      <Header />
+      <Main />
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import './index.css'
-import App from './App'
+import App from './containers/App'
 
 render(<App />, document.getElementById('root'))
