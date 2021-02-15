65fd9fb39707dee740cb4bbd52f705019eda9a95
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-02-15 21:35:53 +0330

    update: classes structure in table data

--- src/components/Main/Table/TableData.jsx
+++ src/components/Main/Table/TableData.jsx
@@ -4,9 +4,7 @@ import DeleteButton from './DeleteButton'
 import EditButton from './EditButton'
 
 const TableData = ({ id, title, amount, type, category, createdAt }) => {
-  // Type
   const typeColor = type === 'income' ? 'green' : 'red'
-  const typeClasses = `px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-${typeColor}-100 text-${typeColor}-800`
 
   return (
     <tr>
@@ -14,13 +12,20 @@ const TableData = ({ id, title, amount, type, category, createdAt }) => {
       <td className="px-6 py-4 whitespace-nowrap">{title}</td>
       <td className="px-6 py-4 whitespace-nowrap">${amount}</td>
       <td className="px-6 py-4 whitespace-nowrap">
-        <span className={typeClasses}>{type}</span>
+        <span
+          className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-${typeColor}-100 text-${typeColor}-800`}
+        >
+          {type}
+        </span>
       </td>
       <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-        {category.map(item => {
-          const classes = `px-2 mr-1 inline-flex text-xs leading-5 font-semibold rounded-full  bg-${item.color}-100 text-${item.color}-800`
-          return <span className={classes}>{item.name}</span>
-        })}
+        {category.map(item => (
+          <span
+            className={`px-2 mr-1 inline-flex text-xs leading-5 font-semibold rounded-full bg-${item.color}-100 text-${item.color}-800`}
+          >
+            {item.name}
+          </span>
+        ))}
       </td>
       <td className="px-6 py-4 whitespace-nowrap">{createdAt}</td>
       <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
