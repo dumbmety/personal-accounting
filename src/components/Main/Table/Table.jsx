f4eb25054bab361b9a357841cd91f4598a2929c1
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-02-15 21:59:03 +0330

    feat: custom scrollbar

--- src/components/Main/Table/Table.jsx
+++ src/components/Main/Table/Table.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import SimpleBar from 'simplebar-react'
+
 import { tableHead, tableData } from '../../../fixtures/table'
 
 import TableHead from './TableHead'
@@ -7,28 +9,34 @@ import TableData from './TableData'
 
 const Table = () => {
   return (
-    <table className="min-w-full divide-y divide-gray-200 shadow-sm border border-gray-200">
-      <thead className="bg-gray-50">
-        <tr>
-          {tableHead.map(item => (
-            <TableHead key={item.id} srOnly={item.srOnly} title={item.title} />
+    <SimpleBar className="w-full">
+      <table className="min-w-full divide-y divide-gray-200 shadow-sm border border-gray-200">
+        <thead className="bg-gray-50">
+          <tr>
+            {tableHead.map(item => (
+              <TableHead
+                key={item.id}
+                srOnly={item.srOnly}
+                title={item.title}
+              />
+            ))}
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200">
+          {tableData.map(item => (
+            <TableData
+              key={item.id}
+              id={item.id}
+              title={item.title}
+              amount={item.amount}
+              type={item.type}
+              category={item.category}
+              createdAt={item.createdAt}
+            />
           ))}
-        </tr>
-      </thead>
-      <tbody className="bg-white divide-y divide-gray-200">
-        {tableData.map(item => (
-          <TableData
-            key={item.id}
-            id={item.id}
-            title={item.title}
-            amount={item.amount}
-            type={item.type}
-            category={item.category}
-            createdAt={item.createdAt}
-          />
-        ))}
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </SimpleBar>
   )
 }
 
