2e8d5017111df01f2ca758308e14dba560c00e3e
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-02-17 16:01:43 +0330

    refactor: main header classes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header className="mb-5">
-      <h1 className="text-5xl font-semibold tracking-tigh py-2">
-        Personal Accounting
-      </h1>
-      <p className="text-lg leading-10 text-gray-600">
+    <header id="main-header">
+      <h1 className="main-header__title">Personal Accounting</h1>
+      <p className="main-header__text">
         Created by{' '}
         <a
-          className="border-b border-gray-300 text-indigo-600 font-semibold hover:border-indigo-400"
+          className="main-header__text--link"
           href="https://github.com/neysidev"
           target="_blank"
           rel="noreferrer"
