9a89b99ecddec3076d719cd44384a8d4400eda7b
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-04-03 23:56:17 +0430

    feat: added colors to categories

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,12 +18,12 @@ const App = () => {
   const handleCloseAddModal = () => setOpenAddModal(false)
 
   const categories = [
-    { key: 0, text: 'All', value: 'all' },
-    { key: 1, text: 'Shopping', value: 'shopping' },
-    { key: 2, text: 'Health', value: 'health' },
-    { key: 3, text: 'Job', value: 'job' },
-    { key: 4, text: 'Fax', value: 'fax' },
-    { key: 5, text: 'Travel', value: 'travel' },
+    { key: 0, text: 'All', value: 'all', color: 'grey' },
+    { key: 1, text: 'Shopping', value: 'shopping', color: 'violet' },
+    { key: 2, text: 'Health', value: 'health', color: 'red' },
+    { key: 3, text: 'Job', value: 'job', color: 'blue' },
+    { key: 4, text: 'Fax', value: 'fax', color: 'black' },
+    { key: 5, text: 'Travel', value: 'travel', color: 'green' },
   ]
 
   useEffect(() => {
